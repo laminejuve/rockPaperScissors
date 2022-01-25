f2c807569095279db4ca01503970b7cb87167274
laminejuve <laminejuvee@gmail.com>
2022-01-25 15:04:53 +0100

    desktop version

---
+++ README.md
@@ -0,0 +1,49 @@
+#   Rock, Paper, Scissors
+
+This is a solution to the [Rock, Paper, Scissors challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Rules](#rules)
+  - [Original](#original)
+  - [Built with](#built-with)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+- Reset the score after refreshing the browser 
+
+### Rules
+
+If the player wins, they gain 1 point. If the computer wins, the player loses one point.
+
+#### Original
+
+- Paper beats Rock
+- Rock beats Scissors
+- Scissors beats Paper
+
+### Screenshot
+
+![](./images/screenShot.jpeg)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
--- script.js
+++ script.js
@@ -1,97 +1,143 @@
-function computerPlay(){
-    let choices = ["paper","rock","scissors"];
-    let computerPlay = choices[Math.floor(Math.random()*choices.length)];
+
+const playerMove = document.querySelectorAll(".play");
+const score = document.querySelector(".number");
+
+playerMove.forEach(element => element.addEventListener('click', function (e) {
+    let playerSelection = e.target.closest(".play").id;
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    score.textContent = player;
+
+    const html = `
+                              <div class="winner">
+                                <h1>YOU PICKED</h1>
+                                <div class="play" id="${playerSelection}">
+                                  <img src="./images/icon-${playerSelection}.svg" alt="" height="50px" width="50px">
+                                </div>
+                              </div>
+                            
+                              <div class="winner">
+                                <h1>${result}</h1>
+                                <div class="playAgain">PLAY AGAIN</div>
+                              </div>
+                              <div class="winner">
+                                 <h1>THE HOUSSE PICKED</h1>
+                                 <div class="play" id="${computerSelection}">
+                                   <img src="./images/icon-${computerSelection}.svg" alt="" height="50px" width="50px">
+                                 </div>
+                              </div>
+                              `
+    document.querySelector(".main .container").style.display = "none";
+    document.querySelector(".main .container2").insertAdjacentHTML('afterbegin', html);
+    document.querySelector(".main .container2").style.display = "flex";
+    const replay = document.querySelector('.playAgain');
+    console.log(replay);
+    replay.addEventListener('click', function (e) {
+        const winner = document.querySelectorAll(".winner");
+        winner.forEach(win => document.querySelector(".main .container2").removeChild(win));
+        document.querySelector(".main .container2").style.display = "none";
+        document.querySelector(".main .container").style.display = "flex";
+    });
+}));
+
+function computerPlay() {
+    let choices = ["paper", "rock", "scissors"];
+    let computerPlay = choices[Math.floor(Math.random() * choices.length)];
     return computerPlay;
 }
 let player = 0;
-let computer =0;
+let computer = 0;
+let result = '';
 function playRound(playerSelection, computerSelection) {
-    let result;
-    if (computerSelection==="scissors"){
-        if (playerSelection === "paper"){
-            result="You Lose! "+computerSelection +" beats "+playerSelection;
-            computer++;
-        } else if (playerSelection === "rock"){
-                   result="You Win! "+playerSelection +" beats "+computerSelection;
-                   player++;
-                }else {
-                   result="this is a Tie! "+playerSelection +" is the same as "+computerSelection;
-                }
+    if (computerSelection === "scissors") {
+        if (playerSelection === "paper") {
+            result = "You Lose! ";
+            // computer++;
+            player--;
+        } else if (playerSelection === "rock") {
+            result = "You Win! ";
+            player++;
+        } else {
+            result = "this is a Tie! ";
+        }
     }
-    if (computerSelection==="paper"){
-        if (playerSelection === "rock"){
-            result="You Lose! "+computerSelection +" beats "+playerSelection;
-            computer++;
-        } else if (playerSelection === "scissors"){
-                   result="You Win! "+playerSelection +" beats "+computerSelection;
-                   player++;
-                }else {
-                   result="this is a Tie! "+playerSelection +" is the same as "+computerSelection;
-                }
+    if (computerSelection === "paper") {
+        if (playerSelection === "rock") {
+            result = "You Lose! ";
+            // computer++;
+            player--;
+        } else if (playerSelection === "scissors") {
+            result = "You Win! ";
+            player++;
+        } else {
+            result = "this is a Tie! ";
+        }
     }
-    if (computerSelection==="rock"){
-        if (playerSelection === "scissors"){
-            result="You Lose! "+computerSelection +" beats "+playerSelection;
-            computer++;
-        } else if (playerSelection === "paper"){
-                   result="You Win! "+playerSelection +" beats "+computerSelection;
-                   player++;
-                }else {
-                   result="this is a Tie! "+playerSelection +" is the same as "+computerSelection;
-                }
+    if (computerSelection === "rock") {
+        if (playerSelection === "scissors") {
+            result = "You Lose! ";
+            // computer++;
+            player--;
+        } else if (playerSelection === "paper") {
+            result = "You Win! ";
+            player++;
+        } else {
+            result = "this is a Tie! ";
+        }
     }
     return result;
 }
-function game(btnName){
-    div1.textContent= playRound(btnName, computerPlay());
-    if (player < 5 && computer <5){
-        div2.textContent="Computer : "+computer+"    player :"+player;
-    }else if (computer < player){
-        div2.textContent="Congratulation!! you Win against the computer "
-        +player+"-"+computer+"  now you can start a new game";
-        player=0;
-        computer =0;
-        }else{
-        div2.textContent="You lose against the computer "
-        +player+"-"+computer+"   now you can start a new game";
-        player=0;
-        computer =0;
+// function game(btnName) {
+//     div1.textContent = playRound(btnName, computerPlay());
+//     if (player < 5 && computer < 5) {
+//         div2.textContent = "Computer : " + computer + "    player :" + player;
+//     } else if (computer < player) {
+//         div2.textContent = "Congratulation!! you Win against the computer "
+//             + player + "-" + computer + "  now you can start a new game";
+//         player = 0;
+//         computer = 0;
+//     } else {
+//         div2.textContent = "You lose against the computer "
+//             + player + "-" + computer + "   now you can start a new game";
+//         player = 0;
+//         computer = 0;
 
-          }
-    div1.style.border = 'solid 5px black';
-    div2.style.border = 'solid 5px black';
-
-
-}
-let btn1 = document.createElement("button");
-btn1.innerHTML = "Rock";
-btn1.name = "rock";
-btn1.onclick = function () {
-    game(btn1.name);
-  };
-document.body.appendChild(btn1);
+//     }
+//     div1.style.border = 'solid 5px black';
+//     div2.style.border = 'solid 5px black';
 
 
-let btn2 = document.createElement("button");
-btn2.innerHTML = "Scissors";
-btn2.name = "scissors"
-btn2.onclick = function () {
-    game(btn2.name);
-  };
-document.body.appendChild(btn2);
+// }
 
-let btn3 = document.createElement("button");
-btn3.innerHTML = "Paper";
-btn3.name = "paper"
-btn3.onclick = function () {
-    game(btn3.name);
-  };
-document.body.appendChild(btn3);
+// let btn1 = document.createElement("button");
+// btn1.innerHTML = "Rock";
+// btn1.name = "rock";
+// btn1.onclick = function () {
+//     game(btn1.name);
+// };
+//document.body.appendChild(btn1);
 
-let div1 = document.createElement('div');
-document.body.appendChild(div1);
-let div2 = document.createElement('div');
-document.body.appendChild(div2);
+
+// let btn2 = document.createElement("button");
+// btn2.innerHTML = "Scissors";
+// btn2.name = "scissors"
+// btn2.onclick = function () {
+//     game(btn2.name);
+// };
+// //document.body.appendChild(btn2);
+
+// let btn3 = document.createElement("button");
+// btn3.innerHTML = "Paper";
+// btn3.name = "paper"
+// btn3.onclick = function () {
+//     game(btn3.name);
+// };
+// //document.body.appendChild(btn3);
+
+// let div1 = document.createElement('div');
+// //document.body.appendChild(div1);
+// let div2 = document.createElement('div');
+// //document.body.appendChild(div2);
 
 
 
